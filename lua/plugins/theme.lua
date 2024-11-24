4e851ed86691fbbe20284804b313ff1ab6187633
test <youxiang@qq.com>
2024-11-25 07:42:48 +0800

    gruvbox的Java关键词高亮已修复

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -21,7 +21,6 @@ return {
       },
       -- 修改Java文件关键词(public static)高亮为红色，不改为黄色
       overrides = {
-        ["@type.qualifier"] = { link = "@keyword" },
         GruvboxRedSign = { fg = "#fb4934", bg = "NONE" },
         GruvboxGreenSign = { fg = "#b8bb26", bg = "NONE" },
         GruvboxBlueSign = { fg = "#83a598", bg = "NONE" },
