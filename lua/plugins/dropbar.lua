92188a49e16756af0e727a0afc6c46bde511c8e8
test <youxiang@qq.com>
2025-03-09 12:12:28 +0800

    控制面包屑导航宽度

--- lua/plugins/dropbar.lua
+++ lua/plugins/dropbar.lua
@@ -1,9 +1,24 @@
+local truncate = true
+
+local sources = {
+  path = {
+    max_depth = 16,
+  },
+}
+if OwnUtil.sys.is_termux() then
+  truncate = false
+  sources.path.max_depth = 1
+end
 return {
   {
     -- 面包屑导航
     "Bekaboo/dropbar.nvim",
     event = "LazyFile",
     opts = {
+      sources = sources,
+      bar = {
+        truncate = truncate,
+      },
       icons = {
         kinds = {
           symbols = OwnUtil.icons.kinds,
