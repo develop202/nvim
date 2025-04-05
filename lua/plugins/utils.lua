053a34baa6925bf21e32fd97ba989f0ca888658c
test <youxiang@qq.com>
2025-04-05 13:15:24 +0800

    切换插件kulala.nvim到主分支

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -64,6 +64,7 @@ return {
   },
   {
     "mistweaverco/kulala.nvim",
+    brains = "main",
     opts = function(_, opts)
       if OwnUtil.sys.is_termux() then
         opts.split_direction = "horizontal"
