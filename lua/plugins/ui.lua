48348430fd24ef109d1fa542709b5b1b2cc771ba
test <youxiang@qq.com>
2025-04-07 16:43:55 +0800

    添加插件nvim-colorizer.lua的生效文件类型

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -17,15 +17,38 @@ return {
     },
     opts = {
       filetypes = {
-        "*", -- Highlight all files, but customize some others.
+        -- "*",
+        "htmldjango",
+        "tsx",
+        "jsx",
+        "erb",
+        "svelte",
+        "vue",
+        "blade",
+        "php",
+        "templ",
+        "astro",
+        "html",
+        "scss",
+        "css",
+        "less",
+        "javascript",
+        "typescript",
+        "json",
+        "yaml",
+        "markdown",
+        "lua",
+        "go",
+        "kotlin",
+        "dart",
         -- 不在cmp补全项里显示
-        "!cmp_menu",
+        -- "!cmp_label",
         -- cmp_menu = {
         --   always_update = true,
         -- },
         -- 不在cmp介绍里面显示
-        "!cmp_docs",
-        "!lazy",
+        -- "!cmp_document",
+        -- "!lazy",
         -- cmp_docs = {
         --   always_update = true,
         -- },
