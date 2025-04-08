622513d3151c08b40d3d8d2748d10abf0b6ebcd6
test <youxiang@qq.com>
2025-04-08 08:16:04 +0800

    删除多余文件

--- after/ftplugin/javascript/opts.lua
+++
@@ -1,7 +0,0 @@
-if OwnUtil.sys.is_termux() then
-  -- 控制cmp补全框宽度
-  vim.g.cmp_widths = {
-    abbr = 25,
-    menu = 30,
-  }
-end
--- after/ftplugin/xml/opts.lua
+++
@@ -1,5 +0,0 @@
--- 控制cmp补全框宽度
-vim.g.cmp_widths = {
-  abbr = 40,
-  menu = 30,
-}
