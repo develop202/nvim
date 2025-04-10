890a601d87bdcb3675ec3aabe7b6217e7607a733
test <youxiang@qq.com>
2025-04-10 17:14:20 +0800

    开启kotlin和php扩展,dart:修复通知图标显示不全的问题

--- lua/plugins/dart.lua
+++ lua/plugins/dart.lua
@@ -5,6 +5,22 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
     },
+    opts = function()
+      local utils = require("flutter-tools.utils")
+      require("flutter-tools.ui").notify = function(msg, level, opts)
+        opts, level = opts or {}, level or require("flutter-tools.ui").INFO
+        msg = type(msg) == "table" and utils.join(msg) or msg
+        if msg == "" then
+          return
+        end
+        local args = { title = "Flutter tools", timeout = opts.timeout, icon = " " }
+        if opts.once then
+          vim.notify_once(msg, level, args)
+        else
+          vim.notify(msg, level, args)
+        end
+      end
+    end,
     config = true,
   },
   {
