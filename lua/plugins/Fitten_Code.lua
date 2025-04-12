391872e3a0e9e07192a3b75ec25f7dbb0189079a
test <youxiang@qq.com>
2025-04-12 22:33:33 +0800

    fitten:添加快捷键。添加新插件Trans.nvim

--- lua/plugins/Fitten_Code.lua
+++ lua/plugins/Fitten_Code.lua
@@ -1,10 +1,39 @@
 return {
   "luozhiya/fittencode.nvim",
   event = "LazyFile",
-  config = function()
-    local API = require("fittencode.api").api
-    local Base = require("fittencode.base")
-    Base.map("i", "<a-down>", API.accept_all_suggestions)
-    require("fittencode").setup()
-  end,
+  opts = {
+    inline_completion = {
+      -- 关闭行内补全
+      enable = false,
+    },
+  },
+  keys = {
+    {
+      "<leader>fae",
+      "<cmd>Fitten enable_completions<cr>",
+      desc = "开启Fitten AI提示",
+    },
+    {
+      "<leader>fad",
+      "<cmd>Fitten disable_completions<cr>",
+      desc = "关闭Fitten AI提示",
+    },
+    {
+      -- 用于句子翻译
+      "<leader>tc",
+      "<cmd>Fitten translate_text_into_chinese<cr>",
+      mode = { "n", "v", "x" },
+      desc = "AI英译汉",
+    },
+    {
+      "<leader>fat",
+      "<cmd>Fitten toggle_chat<cr>",
+      desc = "开启/关闭Fitten AI对话窗口",
+    },
+    {
+      "<leader>fao",
+      "<cmd>Fitten start_chat<cr>",
+      desc = "开始AI对话",
+    },
+  },
 }
