d0f469fd5769dcb379b04f394fb5f4ad4a5f1121
test <youxiang@qq.com>
2025-04-19 23:51:23 +0800

    trans:修改快捷键

--- lua/plugins/trans.lua
+++ lua/plugins/trans.lua
@@ -6,10 +6,10 @@ return {
     end,
     keys = {
       -- 可以换成其他你想映射的键
-      { "<leader>mm", mode = { "n", "x" }, "<Cmd>Translate<CR>", desc = "󰊿 Translate" },
-      { "<leader>mk", mode = { "n", "x" }, "<Cmd>TransPlay<CR>", desc = " Auto Play" },
+      { "<leader>tm", mode = { "n", "x" }, "<Cmd>Translate<CR>", desc = "󰊿 Translate" },
+      { "<leader>tk", mode = { "n", "x" }, "<Cmd>TransPlay<CR>", desc = " Auto Play" },
       -- 目前这个功能的视窗还没有做好，可以在配置里将view.i改成hover
-      { "<leader>mi", "<Cmd>TranslateInput<CR>", desc = "󰊿 Translate From Input" },
+      { "<leader>ti", "<Cmd>TranslateInput<CR>", desc = "󰊿 Translate From Input" },
     },
     dependencies = { "kkharji/sqlite.lua" },
     -- 主要用于单词翻译,没有申请api
